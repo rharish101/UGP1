ac66c257e4fb8d0d47a12ad29dfc4aa283da6638
rharish101 <harish.rajagopals@gmail.com>
2018-11-25 14:35:05 +0530

    Tweaked inference scripts and updated gitignore

--- inference_MAD_SRGAN.sh
+++ inference_MAD_SRGAN.sh
@@ -5,9 +5,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --mode inference \
     --is_training False \
     --task MAD_SRGAN \
-    --batch_size 16 \
-    --input_dir_LR ./data/TESTIMAGES_SAMPLING_PATTERNS_LR/ \
-    --input_dir_HR ./data/TESTIMAGES_SAMPLING_PATTERNS_HR/ \
+    --input_dir_LR ./data/mytests/ \
     --num_resblock 16 \
     --perceptual_mode VGG54 \
     --pre_trained_model True \

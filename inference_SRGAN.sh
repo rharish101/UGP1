4b989a611a0195a7d2c1ce362873769cc3279567
rharish101 <harish.rajagopals@gmail.com>
2018-11-25 14:35:05 +0530

    Tweaked inference scripts and updated gitignore

--- inference_SRGAN.sh
+++ inference_SRGAN.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=0 python main.py \
-    --output_dir ./result/ \
-    --summary_dir ./result/log/ \
+    --output_dir ./result/SRGAN/ \
+    --summary_dir ./result/SRGAN/log/ \
     --mode inference \
     --is_training False \
     --task SRGAN \
-    --input_dir_LR ./data/test_LR/ \
+    --input_dir_LR ./data/mytests/ \
     --num_resblock 16 \
     --perceptual_mode VGG54 \
     --pre_trained_model True \
